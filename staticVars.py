80baaceed0a28b791709de8ae88edd425fac71d7
while0x1 <94356664+while0x1@users.noreply.github.com>
2024-12-05 16:09:02 +0800

    Update staticVars.py

--- staticVars.py
+++ staticVars.py
@@ -1,6 +1,12 @@
-NET = 'TESTNET'
+NET = 'MAINNET'
 USE_BF = False
-BLOCK_FROST_PROJET_ID = 'yourprojectidgoeshere'
-OGMIOS_IP_TNET = "192.168.1.1"
-OGMIOS_IP_MNET = "172.16.1.1"
-SEED = "your seed goes here"
+BLOCK_FROST_PROJECT_ID = 'blockfrostkeygoeshere'
+OGMIOS_IP_TNET = "127.0.0.1"
+OGMIOS_IP_MNET = "127.0.0.1"
+SEED = "seedy seed seed seed seed seed"
+POLICY =  '11cf769ad4ed70cc31579a9c65496807009cfaa5f9a3f5dac1e0b081'
+SCRIPT_HASH = '064f025b69db47523a76e6107314ea184724340d577157f9ee473c41'
+REFSCRIPT_HASH = '1067d7b174a511258d9bcff1ccc856fb1d951cbc5c405ba9948f4914'
+TUNA_CONTRACT = 'addr1w838s2mvyx8wg0p0n9qwl30kyx2murnwxqnwx68am3fldlc5ecphg'
+TUNA_POLICY = b'\xc9\x81\xfc\x98\xe7a\xe3\xbbD\xae5\xe7\xd9z\xe6"\x7fhK\xcboP\xa66u=\xa4\x8e'
+TUNA_NAME = b'TUNA\xe2x+l!\x8e\xe4</\x99@\xef\xc5\xf6!\x95\xbe\x0en0&\xe3h\xfd\xdcS\xf6\xff'
